45adc95e42a0abca4a3b1d0491906df346e86e6a
Shaun <shaun@meshfresh.com>
2017-01-18 16:58:52 -0500

    First Commit

--- archive-work.php
+++ archive-work.php
@@ -1,95 +1,124 @@
-<?php get_header(); ?>
+<?php /* Template Name: Work Archive - Macy */
 
-<section id="works-filter">
-	<div class="container-main">
-		<div class="gutter">
-			<a id="complete-works" href="#">More Projects</a>
-			<ul id="filters">
-			<?php
-				$categories = get_terms('medium',array(
-				 	'orderby'    => 'id',
-				 	'hide_empty' => 0
-				 ));
-				 /*foreach($categories as $cat){
-				 	echo "<li data-filter='".$cat->slug."'>".$cat->name."</li>";
-				 }*/
-			?>
-			</ul>
-			<a id="other-works" href="<?php echo get_permalink(51);  ?>">Case Studies &raquo;</a>
-		</div>
-	</div>
-</section>
-<section id="works-archive">
-	<div class="container-main">
-		<div class="gutter cf">
-			<?php $args = array(
+
+get_header(); ?>
+
+<section id=""><!-- page-content -->
+	<div class="work-grid" id="macy"><!-- container -->
+		<!-- <div class="row"> -->
+	<?php
+		$args = array(
 					'post_type'      => 'work',
-					'meta_key'       => 'featured',
-					'meta_value'     => true,
-					'meta_compare'   => '!=',
-					'posts_per_page' => -1
+					//'meta_key'       => 'featured',
+					//'meta_value'     => true,
+					//'meta_compare'   => '!=',
+					'order' => 'ASC',
+					'order_by' => 'date',
+					'posts_per_page' => -1,
+					'tax_query' => array(
+						array(
+							'taxonomy' => 'display_state',
+							'field'    => 'slug',
+							'terms'    => 'archive',
+							'operator' => 'NOT IN'
+						),
+					),
 				);
 				$query = new WP_Query($args);
-				if ( $query->have_posts() ) { while ( $query->have_posts() ) { $query->the_post(); ?>
+				if ( $query->have_posts() ) { 
+					$wk_ctr=0;
+					while ( $query->have_posts() ) { $query->the_post(); 
 
-				<div class="works-archive-entry">
-					<a href="#<?php echo( basename(get_permalink()) ); ?>"><?php the_title(); ?></a>
-				</div>
+					$wk_ctr++;
+					$tile_image = get_field('featured_cover', $post->ID);
+					$tile_image_url = $tile_image['sizes']['large'];
+					$industries = get_the_terms($post->ID, 'Industry');
+					//var_dump($industries);
+		 			$mediums = get_the_terms($post->ID, 'medium');
+		 			$position = get_field('pt_position');
+		 			//var_dump($mediums);
+		 			$separator = ', ';
+		 			$output_industry = '';
+		 			$output_medium = '';
 
-				<?php } } wp_reset_query(); ?>
-		</div>
-	</div>
+		 			if(!empty($industries)){
+		 				foreach ($industries as $industry){
+		 					//var_dump($industry->name);
+		 					$output_industry .= $industry->name . $separator;
+		 					//var_dump($output_industry);
+		 				}
+		 			}
+
+		 			if(!empty($mediums)){
+		 				foreach ($mediums as $medium){
+		 					//var_dump($industry->name);
+		 					$output_medium .= '<li>' . $medium->name . $separator . '</li>';
+		 					//var_dump(rtrim($output_medium, $separator));
+		 					//var_dump($output_industry);
+		 				}
+		 			}
+
+		 			$rand = mt_rand(1, 3);
+
+		 			// if($rand == 1){
+		 			// 	$position = 'left';
+		 			// }elseif ($rand == 2){
+		 			// 	$position = 'right';
+		 			// }else{
+		 			// 	$position = 'center';
+		 			// }
+					?>
+		 <div class="work-block columns-4"><!-- columns-4 -->
+		 	
+		 	<div class="work-container <?php echo $position; ?>">
+
+		 		<?php 
+
+		 		//var_dump(get_the_post_thumbnail());
+
+		 		if ($tile_image_url != '') { ?>
+		 			<img src="<?php echo $tile_image_url; ?>" >
+		 		<?php }else{ ?>
+
+		 		<?php $rand_img = mt_rand(1, 2); 
+		 			if($rand_img ==1 ){
+	 			?>
+		 			<img class="work-img" src="http://placehold.it/700x805"><!-- if(get_the_post_thumbnail() !== '' ){ }else{echo $tile_image_url; } -->
+	 			<?php }else{ ?>
+	 				<img class="work-img" src="http://placehold.it/700x402">
+	 			<?php } ?>
+	 			<?php } ?>
+
+	 			<div class="hover">
+	 				<div class="content">
+		 				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?> (<?php echo $wk_ctr; ?>)</a></h2>
+		 			<span class="industries tags"> 
+		 				<?php echo rtrim($output_industry, $separator); ?>
+		 			</span>
+	 					<?php //We can bring this in again if we decide to create the individual pages to show this
+	 						//echo get_the_term_list($post->ID, 'Industry','<span class="tags">',', ','</span>') 
+	 					?>
+	 					
+	 					<footer>
+	 						<ul class="mediums tags">
+	 							<?php 
+	 								//Need to figure out how to strip ", " from last li
+	 								echo $output_medium; 
+ 								?>
+ 							</ul>
+	 						<?php 
+	 							//echo get_the_term_list($post->ID,'medium','<ul><li>',',</li><li>', '</li></ul>') 
+	 						?>
+ 						</footer>
+	 					<!-- </ul> -->
+ 					</div><!-- end content -->
+	 			</div><!-- end hover -->
+	 		</div><!-- end work-container -->
+		</div><!-- end work-block -->
+	<?php } } wp_reset_query(); ?>
+	
+	<!-- </div> -->
+</div><!-- end work-grid -->
 </section>
-
-<?php $args = array(
-	'post_type'      => 'work',
-	'meta_key'       => 'featured',
-	'meta_value'     => true,
-	'meta_compare'   => '!=',
-	'posts_per_page' => -1
-);
-$query = new WP_Query($args);
-	if ( $query->have_posts() ) {
-		echo "<section id='works' class='archive'>
-				<div class='container cf'>";
-		while ( $query->have_posts() ) { $query->the_post(); ?>
-
-		<div id="<?php echo( basename(get_permalink()) ); ?>" class="work-post white cf">
-			<div class="work-post-intro">
-				<div class="work-entry-data" style="background:<?php the_field('featured_background'); ?>;">
-					<div class="work-entry-data-vis">
-						<div class="gutter cf">
-							<span class="work-entry-title"><?php the_field('featured_title'); ?></span>
-							<span class="work-entry-medium"><?php the_field('featured_medium'); ?></span>
-						</div>
-					</div>
-					<div class="work-entry-data-add">
-						<div class="gutter cf">
-							<span class="work-entry-statement"><?php the_field('featured_statement'); ?></span>
-						</div>
-					</div>
-				</div>
-
-				<div class="work-post-intro-extra">
-					<div class="gutter cf">
-						<?php the_field('intro_info'); ?>
-					</div>
-				</div>
-			</div>
-
-			<div class="work-post-showcase">
-				<?php $featured = get_field('showcase_images');
-					foreach($featured as $image){
-						echo wp_get_attachment_image($image['image'],'full');
-					} ?>
-			</div>
-
-			<div class="work-post-content">
-				<?php the_content(); ?>
-			</div>
-		</div>
-
-	<?php $i++; } echo "</div>
-				</section>"; } wp_reset_query(); ?>
 
 <?php get_footer(); ?>
