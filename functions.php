d242258099db1c869000afe19342645e2d5c1ba5
Shaun <shaun@meshfresh.com>
2017-02-16 10:49:05 -0500

    Final QA updates ca 20170216

--- functions.php
+++ functions.php
@@ -441,5 +441,9 @@ function get_output_string($post_id,$single){
 
 }
 
+function filter_ptags_on_images($content){
+ return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+}
+add_filter('acf_the_content', 'filter_ptags_on_images', 30);
 
-	
+//remove_filter ('acf_the_content', 'wpautop');
